bd7234e9e295f3d84900aaf60b66442a1f8fbc7f
MoshCohenDev <shiko3011@gmail.com>
2022-04-14 00:27:36 +0300

    change to real api

--- src/service/setDefaultCity.js
+++ src/service/setDefaultCity.js
@@ -1,37 +1,37 @@
-import { locaLeButtonFetch } from "./getYourLocation";
-import { filterCityDaysAndTemperature } from "./logic";
-// import { getCurrentConditions, getLocationForecastName } from "./apiServices";
-import { getCurrentConditions, getLocationForecastName } from "./apiLocal";
+import { locaLeButtonFetch } from './getYourLocation';
+import { filterCityDaysAndTemperature } from './logic';
+import { getCurrentConditions, getLocationForecastName } from './apiServices';
+// import { getCurrentConditions, getLocationForecastName } from "./apiLocal";
 
 export async function getCurrentWeather(defaultCity) {
-  let Key;
-  let name;
-  let location = await locaLeButtonFetch();
-  if (location) {
-    Key = location.Key;
-    name = location.AdministrativeArea.LocalizedName;
-  } else {
-    Key = defaultCity.Key;
-    name = defaultCity.name;
-  }
-  const res = await setCityCurrent(Key, name);
-  return res;
+	let Key;
+	let name;
+	let location = await locaLeButtonFetch();
+	if (location) {
+		Key = location.Key;
+		name = location.AdministrativeArea.LocalizedName;
+	} else {
+		Key = defaultCity.Key;
+		name = defaultCity.name;
+	}
+	const res = await setCityCurrent(Key, name);
+	return res;
 }
 
 const setCityCurrent = async (Key, name) => {
-  const days = await getLocationForecastName(Key);
+	const days = await getLocationForecastName(Key);
 
-  const currentConditions = getCurrentConditions(Key);
-  let currentWeather = {
-    Key,
-    name,
-    isFavorite: false,
-    currentConditions: currentConditions[0],
-  };
-  const daysForecast = await filterCityDaysAndTemperature({
-    days,
-    name: name,
-    key: Key,
-  });
-  return { daysForecast, currentWeather };
+	const currentConditions = getCurrentConditions(Key);
+	let currentWeather = {
+		Key,
+		name,
+		isFavorite: false,
+		currentConditions: currentConditions[0],
+	};
+	const daysForecast = await filterCityDaysAndTemperature({
+		days,
+		name: name,
+		key: Key,
+	});
+	return { daysForecast, currentWeather };
 };
